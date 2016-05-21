97596990684d963449512eba61372599a9c972d7
Andre Holman <andreholman510@gmail.com>
2016-05-21 10:32:04 -0700

    Fix save.js

--- save.js
+++ save.js
@@ -1,6 +1,8 @@
 function save(id) {
 	if (typeof(Storage) !== "undefined") {
-		localStorage.html = document.getElementById(id);
+		localStorage.html = document.getElementById(id).innerHTML;
+		localStorage.css = document.getElementById(id).innerHTML;
+		localStorage.js = document.getElementById(id).innerHTML;
 	} else {
 		alert("Your browser can't save code.");
 	}

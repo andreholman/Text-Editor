7b4842bb3e630d5257ac6ba08ebef31b71325b97
Andre Holman <andreholman510@gmail.com>
2017-01-21 13:22:43 -0800

    Delete save.js

--- save.js
+++
@@ -1,9 +0,0 @@
-function save(id) {
-	if (typeof(Storage) !== "undefined") {
-		localStorage.html = document.getElementById(id).innerHTML;
-		localStorage.css = document.getElementById(id).innerHTML;
-		localStorage.js = document.getElementById(id).innerHTML;
-	} else {
-		alert("Your browser can't save code.");
-	}
-}
